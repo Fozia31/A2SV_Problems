4defa827b3611c692f7091fac656fc8204379e7c
Fozia31 <foziyadamtew00@gmail.com>
2025-02-20 01:05:32 -0800

    Time: 30 ms (65.38%), Space: 21 MB (51.21%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        dict_ = defaultdict( int )
+        dict_[0] = 1
+        count = prefix_sum = 0
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            rem_ = prefix_sum - goal
+            
+            count += dict_[rem_]
+    
+            dict_[prefix_sum] += 1
+        return count
+        
