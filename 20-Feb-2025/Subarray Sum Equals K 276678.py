bf33597df5f2e58df94349be6c260212b5c88b70
Fozia31 <foziyadamtew00@gmail.com>
2025-02-20 03:12:28 -0800

    Created 'Subarray Sum Equals K' by A2SV Hub

---
+++ 20-Feb-2025/Subarray Sum Equals K 276678.py
@@ -0,0 +1,15 @@
+# Problem: Subarray Sum Equals K - https://leetcode.com/problems/subarray-sum-equals-k/
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        dict_ = defaultdict(int)
+        dict_[0] = 1
+        count = prefix_sum = 0
+        for i in range(len(nums)):
+            prefix_sum += nums[i]
+            rem_ = prefix_sum - k
+
+            count += dict_[rem_]
+
+            dict_[prefix_sum] += 1
+        return count
