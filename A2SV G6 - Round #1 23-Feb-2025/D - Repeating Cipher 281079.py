32dcc5977b64f6b61f8b41a5dbbca4373bc64b5d
Fozia31 <foziyadamtew00@gmail.com>
2025-02-23 04:41:45 -0800

    Created 'D - Repeating Cipher' by A2SV Hub

---
+++ A2SV G6 - Round #1 23-Feb-2025/D - Repeating Cipher 281079.py
@@ -0,0 +1,13 @@
+# Problem: D - Repeating Cipher - https://codeforces.com/gym/585107/problem/D
+
+n =int(input())
+s = input()
+output = ""
+output = ""
+count = 0
+i = 1
+while count < len(s):
+    output += s[count]
+    count += i
+    i += 1
+print(output)
