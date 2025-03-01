1d3e1d2206a7b0aa58fc4240e8cf9ade89840038
Fozia31 <foziyadamtew00@gmail.com>
2025-03-01 05:33:06 -0800

    Created 'Row With Maximum Ones' by A2SV Hub

---
+++ 01-Mar-2025/Row With Maximum Ones 287633.py
@@ -0,0 +1,15 @@
+# Problem: Row With Maximum Ones - https://leetcode.com/problems/row-with-maximum-ones/
+
+class Solution:
+    def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
+        max_ones = -1
+        row_index = -1
+        
+        for i in range(len(mat)):
+            count_ones = mat[i].count(1)
+            
+            if count_ones > max_ones:
+                max_ones = count_ones
+                row_index = i
+        
+        return [row_index, max_ones]
