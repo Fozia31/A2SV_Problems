0649cb08141e03abcae173e111c9bf5519c67c85
Fozia31 <foziyadamtew00@gmail.com>
2025-03-11 00:21:37 -0700

    Created 'Array Splitting' by A2SV Hub

---
+++ 11-Mar-2025/Array Splitting 301153.py
@@ -0,0 +1,12 @@
+# Problem: Array Splitting - https://codeforces.com/problemset/problem/1197/C
+
+m , n = map(int , input().split())
+a = list(map(int , input().split()))
+ans = [] 
+for i in range(1 ,len(a)):
+    ans.append(a[i] - a[i - 1])
+ans.sort(reverse= True)
+sum_ = sum(ans)  - sum(ans[:n-1])
+
+print(sum_)
+     
