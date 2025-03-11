fc2354c8f570e28452adb2b339fe8c3ad4cf8848
Fozia31 <foziyadamtew00@gmail.com>
2025-03-11 04:37:41 -0700

    Created 'Alternating Subsequence' by A2SV Hub

---
+++ 11-Mar-2025/Alternating Subsequence 302327.py
@@ -0,0 +1,22 @@
+# Problem: Alternating Subsequence - https://codeforces.com/contest/1343/problem/C
+
+n = int(input())
+for i in range(n):
+    m = int(input())
+    a = list(map(int , input().split()))
+    left , right= 0 , 0
+    max_val , max_sum = 0,0
+    while left < m:
+        
+        max_val = a[left]
+        flag = a[left] > 0
+        
+        while right < m and (a[right] > 0) == flag:
+            max_val = max(max_val , a[right])
+            right += 1
+        
+        max_sum += max_val
+        left = right
+    print(max_sum)
+         
+        
