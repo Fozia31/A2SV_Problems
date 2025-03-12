cb1aad1ce83b9117c92be1ec536646120592581c
Fozia31 <foziyadamtew00@gmail.com>
2025-03-12 06:45:32 -0700

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 12-Mar-2025/Pascal's Triangle II - LeetCode 303916.py
@@ -0,0 +1,21 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex == 0:
+            return [1]
+        if rowIndex == 1:
+            return [1 ,1]
+        # if rowIndex == 2:
+        #     return [1,2,1]
+
+        p_row = self.getRow( rowIndex - 1)
+        ans = [1]
+
+        for i in range(rowIndex - 1):
+            ans.append( p_row[i] + p_row[i + 1])
+
+        ans.append(1)
+
+        return ans
+        
