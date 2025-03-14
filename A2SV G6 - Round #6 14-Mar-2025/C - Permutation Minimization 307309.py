fb4aae3830c801de96e4d0a0e73eec583abf9f28
Fozia31 <foziyadamtew00@gmail.com>
2025-03-14 11:05:00 -0700

    Created 'C - Permutation Minimization' by A2SV Hub

---
+++ A2SV G6 - Round #6 14-Mar-2025/C - Permutation Minimization 307309.py
@@ -0,0 +1,20 @@
+# Problem: C - Permutation Minimization - https://codeforces.com/gym/594077/problem/C
+
+from collections import deque
+
+x = int(input())
+for i in range(x):
+   m = int(input())
+   a = list(map(int,input().split()))
+   que = deque()
+   que.append(a[0])
+   for i in range(1 , m):
+
+       if a[i] < que[0]:
+           que.appendleft(a[i])
+       else:
+           que.append(a[i])
+   print(*(que))
+
+
+
