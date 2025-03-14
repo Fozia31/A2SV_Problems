35128c4e0b991ed88bbbd12a4e6cd12c9b936c5c
Fozia31 <foziyadamtew00@gmail.com>
2025-03-14 11:05:23 -0700

    Created 'E - Minimum Subsequence' by A2SV Hub

---
+++ A2SV G6 - Round #6 14-Mar-2025/E - Minimum Subsequence 307312.py
@@ -0,0 +1,33 @@
+# Problem: E - Minimum Subsequence - https://codeforces.com/gym/594077/problem/E
+
+ts = int(input())
+for _ in range(ts):
+    n = int(input())
+    s = input()
+    sq = 1
+    sequences = {"0": [], "1":[]}
+    stack = []
+    ans = [0] * n
+    for i in range(n):
+        if stack:
+            if stack[-1] == s[i]:
+                sequences[stack[-1]].append(ans[i-1])
+                if s[i] == "0":
+                    if sequences["1"]:
+                        ans[i] = sequences["1"].pop()
+                    else:
+                        sq += 1
+                        ans[i] = sq
+                elif sequences["0"]:
+                    ans[i] = sequences["0"].pop()
+                else:
+                    sq += 1
+                    ans[i] = sq
+            else:
+                ans[i] = ans[i-1]
+        else:
+            ans[i] = sq
+        stack.append(s[i])
+    print(sq)
+    print(*ans)
+                
