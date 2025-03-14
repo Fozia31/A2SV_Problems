bb8b8e2fa13e554aabe6430356311856a3e5e9e4
Fozia31 <foziyadamtew00@gmail.com>
2025-03-14 11:05:08 -0700

    Created 'D - Meklit's Chat Screen' by A2SV Hub

---
+++ A2SV G6 - Round #6 14-Mar-2025/D - Meklit's Chat Screen 307310.py
@@ -0,0 +1,23 @@
+# Problem: D - Meklit's Chat Screen - https://codeforces.com/gym/594077/problem/D
+
+from collections import deque
+n , k = map(int , input().split())
+a = list(map(int , input().split()))
+que= deque()
+s =set()
+for i in range(n):
+    if que and a[i] in s :
+        continue
+    elif len(que) < k: 
+        s.add(a[i])
+        que.append(a[i])
+    else:
+        que.append(a[i])
+        s.add(a[i])
+        t = que.popleft()
+        s.discard(t)
+print(len(que))
+res = []
+for j in range(len(que) - 1 , -1 , -1 ):
+    res.append(que[j])
+print(*(res))
