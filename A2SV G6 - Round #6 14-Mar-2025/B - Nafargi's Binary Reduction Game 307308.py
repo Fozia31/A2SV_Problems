552569f4d7aa81e47600bdcc4fb535927f569eda
Fozia31 <foziyadamtew00@gmail.com>
2025-03-14 11:04:48 -0700

    Created 'B - Nafargi's Binary Reduction Game' by A2SV Hub

---
+++ A2SV G6 - Round #6 14-Mar-2025/B - Nafargi's Binary Reduction Game 307308.py
@@ -0,0 +1,13 @@
+# Problem: B - Nafargi's Binary Reduction Game - https://codeforces.com/gym/594077/problem/B
+
+n = int(input())
+m = input()
+stack = []
+ans = n
+for i in range(n):
+    if stack and ((stack[-1] == "0" and m[i] == "1" ) or (m[i] == "0" and stack[-1] == "1")) :
+        stack.pop()
+        ans -= 2
+    else:
+        stack.append(m[i])
+print(ans)
