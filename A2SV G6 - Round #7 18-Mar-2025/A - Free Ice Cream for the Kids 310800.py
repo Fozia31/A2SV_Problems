0be43cd10b7b26ca9da1debab7d039ccaa2e0a8b
Fozia31 <foziyadamtew00@gmail.com>
2025-03-18 04:44:24 -0700

    Created 'A - Free Ice Cream for the Kids' by A2SV Hub

---
+++ A2SV G6 - Round #7 18-Mar-2025/A - Free Ice Cream for the Kids 310800.py
@@ -0,0 +1,16 @@
+# Problem: A - Free Ice Cream for the Kids - https://codeforces.com/gym/596141/problem/A
+
+n  , m = map(int , input().split())
+left = 0
+rec = m
+for i in range(n) :
+    x , y = map(str , input().split())
+    if x == "-":
+        if rec >= int(y):
+            rec -= int(y)
+        else:
+            left += 1
+    elif x == "+":
+        rec +=int( y)
+print(*([rec , left]))
+        
