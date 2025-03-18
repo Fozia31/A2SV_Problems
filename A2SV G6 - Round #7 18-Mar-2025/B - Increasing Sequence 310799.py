68dce87fc1196ab82fb13a0979663fedd392175e
Fozia31 <foziyadamtew00@gmail.com>
2025-03-18 04:44:11 -0700

    Created 'B - Increasing Sequence' by A2SV Hub

---
+++ A2SV G6 - Round #7 18-Mar-2025/B - Increasing Sequence 310799.py
@@ -0,0 +1,20 @@
+# Problem: B - Increasing Sequence - https://codeforces.com/gym/596141/problem/B
+
+n = int(input())
+for i in range(n):
+    m = int(input())
+    arr = list(map(int , input().split()))
+    b = [0]*m
+    if arr[0] == 1:
+        b[0] = 2
+    else:
+        b[0] = 1
+        
+    for i in range(1 , m):
+        if (b[i -1] + 1) == arr[i]:
+            b[i] = b[i - 1] + 2
+        else:
+            b[i] = b[i - 1] + 1
+            
+    print(b[-1])
+    
