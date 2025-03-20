1e053f78a51bf3c5ad904509f74c064ded2c4573
Fozia31 <foziyadamtew00@gmail.com>
2025-03-20 12:16:04 -0700

    Created 'Subsets' by A2SV Hub

---
+++ 20-Mar-2025/Subsets 313685.py
@@ -0,0 +1,18 @@
+# Problem: Subsets - https://leetcode.com/problems/subsets/
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        sett = []
+        
+        def backtrack(i , com):
+
+            sett.append(com[:])
+
+            for j in range(i , len(nums)):
+                com.append(nums[j])
+                backtrack(j + 1 , com)
+                com.pop()
+            
+        backtrack( 0 , [])
+        return sett
+        
