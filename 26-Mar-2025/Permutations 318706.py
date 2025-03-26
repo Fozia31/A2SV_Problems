1b536ef2aa61bb37de5acfe79915d6c98a53f687
Fozia31 <foziyadamtew00@gmail.com>
2025-03-26 06:39:34 -0700

    Created 'Permutations' by A2SV Hub

---
+++ 26-Mar-2025/Permutations 318706.py
@@ -0,0 +1,24 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+
+        def backtrack( combination):
+            if len(combination) == len(nums):
+                ans.append(combination[:])
+                return
+
+            for i in range(len(nums)):
+                if nums[i] in combination:
+                    continue
+
+                combination.append(nums[i])
+                backtrack(combination)
+                combination.pop()
+        
+        backtrack([])
+        return ans
+
+
+        
