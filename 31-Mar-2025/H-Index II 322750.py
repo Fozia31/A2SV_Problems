1fee535ad144168bee9dff80c004428c8c04ec01
Fozia31 <foziyadamtew00@gmail.com>
2025-03-31 10:51:07 -0700

    Created 'H-Index II' by A2SV Hub

---
+++ 31-Mar-2025/H-Index II 322750.py
@@ -0,0 +1,12 @@
+# Problem: H-Index II - https://leetcode.com/problems/h-index-ii/description/
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n=len(citations)
+        count=0
+        for i in range(n):
+            if citations[i]>=n-i:
+                count=n-i
+                break
+        return count
