c897e375ff408282a1b02ce22d0d6c5b9d34cb04
Fozia31 <foziyadamtew00@gmail.com>
2025-04-19 09:30:50 -0700

    Created 'Number of Good Pairs' by A2SV Hub

---
+++ 19-Apr-2025/Number of Good Pairs 337375.py
@@ -0,0 +1,14 @@
+# Problem: Number of Good Pairs - https://leetcode.com/problems/number-of-good-pairs/
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        answer = 0
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if i == j:
+                    continue
+                else:
+                    if nums[i] == nums[j] and i < j:
+                        answer += 1
+        return answer
+        
