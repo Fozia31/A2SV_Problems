af553b5316015c3fdea28631e14754f7b268e27d
Fozia31 <foziyadamtew00@gmail.com>
2025-04-21 04:29:48 -0700

    Created 'Find Common Characters' by A2SV Hub

---
+++ 21-Apr-2025/Find Common Characters 338079.py
@@ -0,0 +1,22 @@
+# Problem: Find Common Characters - https://leetcode.com/problems/find-common-characters/
+
+class Solution:
+    def commonChars(self, words: List[str]) -> List[str]:
+        Count = Counter(words[0])
+
+        for word in words[1:]:
+            tmp_count = Counter(word)
+            
+            for key , value in Count.items():
+                Count[key] = min(value , tmp_count[key])
+
+        answer = []
+        for key , value in Count.items():
+            for _ in range(value):
+                answer.append(key)
+        
+        return answer
+
+
+
+            
