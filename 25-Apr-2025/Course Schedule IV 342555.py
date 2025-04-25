932478394ded50d68c0504198ee12d23efd97013
Fozia31 <foziyadamtew00@gmail.com>
2025-04-25 11:23:07 -0700

    Created 'Course Schedule IV' by A2SV Hub

---
+++ 25-Apr-2025/Course Schedule IV 342555.py
@@ -0,0 +1,30 @@
+# Problem: Course Schedule IV - https://leetcode.com/problems/course-schedule-iv/description/
+
+class Solution(object):
+    def checkIfPrerequisite(self, numCourses, prerequisites, queries):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :type queries: List[List[int]]
+        :rtype: List[bool]
+        """
+        graph = [set() for _ in range(numCourses)]
+        for pre , course in prerequisites:
+            graph[pre].add(course)
+
+        for i in range(numCourses):
+            for j in range(numCourses):
+                if i in graph[j]:
+                    graph[j].update(graph[i])
+
+        answer = []
+        for u , v in queries :
+            if v in graph[u]:
+                answer.append(True)
+            else:
+                answer.append(False)
+
+        return answer
+
+
+        
