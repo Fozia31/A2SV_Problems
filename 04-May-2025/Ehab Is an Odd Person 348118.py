01b55ff544e6a0c14d9902c0f8075277727e8fd8
Fozia31 <foziyadamtew00@gmail.com>
2025-05-04 09:48:33 -0700

    Created 'Ehab Is an Odd Person' by A2SV Hub

---
+++ 04-May-2025/Ehab Is an Odd Person 348118.py
@@ -0,0 +1,18 @@
+# Problem: Ehab Is an Odd Person - https://codeforces.com/problemset/problem/1174/B
+
+n=int(input())
+a=list(map(int,input().split()))
+even_checker = False
+odd_checker = False
+for x in a:
+    if x%2 == 1:
+        odd_checker =True
+    else:
+        even_checker = True
+        
+if even_checker and odd_checker:
+    print(*(sorted(a)))
+else:
+    print(*a)
+        
+        
