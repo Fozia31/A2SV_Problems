56f3231c0b2e3fc5016590af8eac35d0d608b852
Fozia31 <foziyadamtew00@gmail.com>
2025-10-23 19:18:53 +0400

    Created 'Dreamoon and Stairs' by A2SV Hub

---
+++ 23-Oct-2025/Dreamoon and Stairs 405295.py
@@ -0,0 +1,16 @@
+# Problem: Dreamoon and Stairs - https://codeforces.com/problemset/problem/476/A
+
+n , m = map(int, input().split())
+
+min_move = (n + 1)//2
+
+if min_move%m == 0:
+    ans = min_move
+else:
+    ans = ((min_move + m -1)//m)*m
+
+
+if ans > n:
+    print(-1)
+else:
+    print(ans)
