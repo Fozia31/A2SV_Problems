8be0f3355d21c509909ef93072521f7ae0004614
Fozia31 <foziyadamtew00@gmail.com>
2025-10-23 19:19:02 +0400

    Created 'Minimum Integer' by A2SV Hub

---
+++ 23-Oct-2025/Minimum Integer 405296.py
@@ -0,0 +1,9 @@
+# Problem: Minimum Integer - https://codeforces.com/problemset/problem/1101/A
+
+n = int(input())
+for _ in range(n):
+    l, r, d = map(int, input().split())
+    if d<l:
+        print(d)
+    else:
+        print((r//d + 1)*d)
