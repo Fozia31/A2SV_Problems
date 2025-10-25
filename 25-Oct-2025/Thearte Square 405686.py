8a040bc946d9f78992bdeff6b0fbe95c42923e7c
Fozia31 <foziyadamtew00@gmail.com>
2025-10-25 18:32:41 +0400

    Created 'Thearte Square' by A2SV Hub

---
+++ 25-Oct-2025/Thearte Square 405686.py
@@ -0,0 +1,6 @@
+# Problem: Thearte Square - https://codeforces.com/problemset/problem/1/A
+
+n , m, a = map(int , input().split())
+remining_width =(n + a - 1) // a
+remining_height = (m + a - 1) // a
+print(remining_height*remining_width)
