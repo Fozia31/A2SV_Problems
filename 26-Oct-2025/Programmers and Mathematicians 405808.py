8fb38fc77f70f59bfdf1dad2b68b8d9f39af7e53
Fozia31 <foziyadamtew00@gmail.com>
2025-10-26 17:23:09 +0400

    Created 'Programmers and Mathematicians' by A2SV Hub

---
+++ 26-Oct-2025/Programmers and Mathematicians 405808.py
@@ -0,0 +1,21 @@
+# Problem: Programmers and Mathematicians - https://codeforces.com/problemset/problem/1611/B
+
+n = int(input())
+
+for _ in range(n):
+    a , b = map(int , input().split())
+    if a == b:
+        print(a//2)
+    else:
+        team = (a + b)//4
+        if a < b:
+            if team > a:
+                print(a)
+            else:
+                print(team)
+        else:
+            if team > b:
+                print(b)
+            else:
+                print(team)
+
