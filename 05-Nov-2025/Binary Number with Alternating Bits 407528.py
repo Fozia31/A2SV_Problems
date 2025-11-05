71ac1aed7c3697b2970c9ac51a6892029caba6b6
Fozia31 <foziyadamtew00@gmail.com>
2025-11-05 20:02:30 +0400

    Created 'Binary Number with Alternating Bits' by A2SV Hub

---
+++ 05-Nov-2025/Binary Number with Alternating Bits 407528.py
@@ -0,0 +1,19 @@
+# Problem: Binary Number with Alternating Bits - https://leetcode.com/problems/binary-number-with-alternating-bits/
+
+class Solution(object):
+    def hasAlternatingBits(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        prev_bit = n & 1
+        n >>= 1
+        
+        while n > 0:
+            curr_bit = n & 1
+            if curr_bit == prev_bit:
+                return False
+            prev_bit = curr_bit
+            n >>= 1
+        
+        return True
