bdfd01da6418558f699a8f598e7b965602fbcd78
Fozia31 <foziyadamtew00@gmail.com>
2025-11-13 12:32:39 +0400

    Created 'C - The Splitting Game' by A2SV Hub

---
+++ A2SV G6 - Round #2 13-Nov-2025/C - The Splitting Game 408732.py
@@ -0,0 +1,25 @@
+# Problem: C - The Splitting Game - https://codeforces.com/gym/586960/problem/C
+
+from collections import Counter
+
+
+n = int(input())
+
+for _ in range(n):
+    t = int(input())
+    s = input()
+
+    counter = Counter(s)
+    counter_left = {}
+    res = 0
+    for st in s:
+        counter[st] -= 1
+        if st not in counter_left:
+            counter_left[st] = 1
+        else:
+            counter_left[st] += 1
+        if counter[st] == 0:
+            del counter[st]
+        res = max(res, len(counter_left) + len(counter))
+
+    print(res)
