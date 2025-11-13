7788141d2039de6edb749996b4e3bed0b767c4d6
Fozia31 <foziyadamtew00@gmail.com>
2025-11-13 12:35:16 +0400

    Created 'D - Bomb Detection Validation' by A2SV Hub

---
+++ A2SV G6 - Round #2 13-Nov-2025/D - Bomb Detection Validation 408733.py
@@ -0,0 +1,31 @@
+# Problem: D - Bomb Detection Validation - https://codeforces.com/gym/586960/problem/D
+
+n, m = map(int, input().split())
+field = [input().strip() for _ in range(n)]
+
+dx = [-1, -1, -1, 0, 0, 1, 1, 1]
+dy = [-1, 0, 1, -1, 1, -1, 0, 1]
+
+valid = True
+
+for i in range(n):
+    for j in range(m):
+        if field[i][j] == '*':
+            continue
+        count = 0
+        for k in range(8):
+            ni, nj = i + dx[k], j + dy[k]
+            if 0 <= ni < n and 0 <= nj < m and field[ni][nj] == '*':
+                count += 1
+        if field[i][j] == '.':
+            if count != 0:
+                valid = False
+                break
+        else:
+            if int(field[i][j]) != count:
+                valid = False
+                break
+    if not valid:
+        break
+
+print("YES" if valid else "NO")
