ef4f5a2378fcd6580189ee0991bc98da52238b52
Fozia31 <foziyadamtew00@gmail.com>
2025-11-13 12:43:25 +0400

    Created 'B - Special Matrix Quest' by A2SV Hub

---
+++ A2SV G6 - Round #2 13-Nov-2025/B - Special Matrix Quest 408734.py
@@ -0,0 +1,14 @@
+# Problem: B - Special Matrix Quest - https://codeforces.com/gym/586960/problem/B
+
+n = int(input())
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+good_sum = 0
+mid = n // 2 
+
+for i in range(n):
+    for j in range(n):
+        if i == j or i + j == n - 1 or i == mid or j == mid:
+            good_sum += matrix[i][j]
+
+print(good_sum)
