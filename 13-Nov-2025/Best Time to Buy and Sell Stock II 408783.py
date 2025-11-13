a7973782b1318bc562a64379f2dcad8e36609a15
Fozia31 <foziyadamtew00@gmail.com>
2025-11-13 22:06:21 +0400

    Created 'Best Time to Buy and Sell Stock II' by A2SV Hub

---
+++ 13-Nov-2025/Best Time to Buy and Sell Stock II 408783.py
@@ -0,0 +1,13 @@
+# Problem: Best Time to Buy and Sell Stock II - https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
+        return profit
