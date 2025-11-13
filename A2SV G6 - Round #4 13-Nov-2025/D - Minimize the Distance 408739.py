bc4477f10d43157c1481e40f900c4c8d1d410ea8
Fozia31 <foziyadamtew00@gmail.com>
2025-11-13 13:58:18 +0400

    Created 'D - Minimize the Distance' by A2SV Hub

---
+++ A2SV G6 - Round #4 13-Nov-2025/D - Minimize the Distance 408739.py
@@ -0,0 +1,11 @@
+# Problem: D - Minimize the Distance - https://codeforces.com/gym/590053/problem/D
+
+n = int(input())
+a = list(map(int, input().split()))
+
+a.sort()  
+
+if n % 2 != 0:
+    print(a[n // 2]) 
+else:
+    print(a[n // 2 - 1])  
