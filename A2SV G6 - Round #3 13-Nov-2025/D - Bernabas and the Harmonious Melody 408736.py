6e18626b10ce32cb0f203bcb7560fb2c22042729
Fozia31 <foziyadamtew00@gmail.com>
2025-11-13 12:58:34 +0400

    Created 'D - Bernabas and the Harmonious Melody' by A2SV Hub

---
+++ A2SV G6 - Round #3 13-Nov-2025/D - Bernabas and the Harmonious Melody 408736.py
@@ -0,0 +1,37 @@
+# Problem: D - Bernabas and the Harmonious Melody - https://codeforces.com/gym/588468/problem/D
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    s = input()
+
+    import math
+    ans = math.inf
+
+    for c in set(s):
+        left = 0
+        right = n - 1
+        cnt = 0
+        possible = True
+
+        while left < right:
+            if s[left] == s[right]:
+                left += 1
+                right -= 1
+            elif s[left] == c:
+                cnt += 1
+                left += 1
+            elif s[right] == c:
+                cnt += 1
+                right -= 1
+            else:
+                possible = False
+                break
+
+        if possible:
+            ans = min(ans, cnt)
+
+    if ans == math.inf:
+        print(-1)
+    else:
+        print(ans)
